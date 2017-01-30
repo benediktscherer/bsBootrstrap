0a30b9db76c3b21cd329b093f0a4785aa55b802d
Benedikt Scherer <benedikt.scherer@talentsconnect.com>
2017-01-30 17:20:12 +0100

    add tools / mixins

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  console.log('Hello World! MAMA!');
+  
 });
 $(document).ready(function(){}),$(document).ready(function(){var d=$(".detect_device div:visible").attr("data-device");$("body").addClass(d),$("p").text("detected dreakpoint width: "+d)});
 $(document).ready( function(){
